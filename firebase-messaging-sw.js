b331fa30de6399456e93cc1cf616f4f75d7e98c8
dmsy-1205 <dshappilyeverafter@gmail.com>
2026-06-29 00:58:05 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,11 @@
 // =================================================================
 // [마스터] 파이어베이스 백그라운드 알림 문지기 (Service Worker)
 // =================================================================
-
+<script src="config.js"></script>
 // 1. 파이어베이스 본부에서 필요한 도구 로드
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// 2. 내 파이어베이스 고유 주소 설정 (★여기에 본인의 키값을 넣으세요★)
-const firebaseConfig = {
-  apiKey: "AIzaSyDMVl65SWhqPcxrMY7h_ir7OgPbk7P1LAs",
-  authDomain: "our-baby-care.firebaseapp.com",
-  projectId: "our-baby-care",
-  storageBucket: "our-baby-care.appspot.com",
-  messagingSenderId: "564751165",
-  appId: "1:564751165:web:12012e95e1240e87e27354"
-};
 
 // 3. 파이어베이스 실행 및 메시징 기능 연결
 firebase.initializeApp(firebaseConfig);
